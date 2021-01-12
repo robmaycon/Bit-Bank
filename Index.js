be8a59e07d6f23d40727b8c24fd96d7e7186d817
Roberto Maycon <robertomaycon@hotmail.com>
2021-01-12 17:42:50 +1300

    Starting Project

---
+++ Index.js
@@ -0,0 +1,9 @@
+const client1Name = 'Roberto';
+const client1Id = 11223344;
+const client1Batch = 1001;
+const clent1Balance = 0;
+
+const client2Name = 'Rose';
+const client2Id = 44332211;
+const client2Batch = 1001;
+const clent2Balance = 0;
