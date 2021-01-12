80e1aeb0d0c55b492b2c7ca1961c1e59a61ef1fe
Roberto Maycon <robertomaycon@hotmail.com>
2021-01-12 19:36:00 +1300

    class account, functions deposit and withdrawn and privete field

--- Index.js
+++ Index.js
@@ -1,23 +1,52 @@
 class Client {
     name;
     id;
-    batch;
-    balance;
-
+  
 };
 
+class Account {
+    #balance = 0;
+    batch;
+
+    withdrawn(value) {
+        if (this.#balance >= value){
+            this.#balance -= value;
+        };
+    };
+
+    deposit(value) {
+        if (value > 0){
+            this.#balance += value;
+        };
+    };
+};
+
+// initial balance
+const robertoAccount = new Account();
+robertoAccount.batch = 1001;
+console.log(robertoAccount);
+
+// Deposit
+robertoAccount.deposit(100);
+console.log(robertoAccount);
+
+//withdrawn
+robertoAccount.withdrawn(50);
+console.log(robertoAccount);
+
+
+
+
+
 const client1 = new Client();
+client1.name = 'Roberto';
+client1.id = 11223344;
+
+
 const client2 = new Client();
-
- client1.name = 'Roberto';
- client1.id = 11223344;
- client1.batch = 1001;
- client1.balance = 0; 
-
  client2.name = 'Rose';
  client2.id = 44332211;
- client2.batch = 1001;
- client2.balance = 0;
 
- console.log(client1);
- console.log(client2);
+
+ //console.log(client1);
+ //console.log(client2);
