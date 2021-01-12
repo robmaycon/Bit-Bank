37cf949b5fa3b88185995df353075225fea75d46
Roberto Maycon <robertomaycon@hotmail.com>
2021-01-12 18:08:35 +1300

    Building first class

--- Index.js
+++ Index.js
@@ -1,9 +1,23 @@
-const client1Name = 'Roberto';
-const client1Id = 11223344;
-const client1Batch = 1001;
-const clent1Balance = 0;
+class Client {
+    name;
+    id;
+    batch;
+    balance;
 
-const client2Name = 'Rose';
-const client2Id = 44332211;
-const client2Batch = 1001;
-const clent2Balance = 0;
+};
+
+const client1 = new Client();
+const client2 = new Client();
+
+ client1.name = 'Roberto';
+ client1.id = 11223344;
+ client1.batch = 1001;
+ client1.balance = 0; 
+
+ client2.name = 'Rose';
+ client2.id = 44332211;
+ client2.batch = 1001;
+ client2.balance = 0;
+
+ console.log(client1);
+ console.log(client2);
