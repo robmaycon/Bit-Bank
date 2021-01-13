04bd1c714354cb74135e20cfaeab4a027ce56b1e
Roberto Maycon <robertomaycon@hotmail.com>
2021-01-13 18:34:43 +1300

    new modules

---
+++ Account.js
@@ -0,0 +1,18 @@
+
+export class Account {
+    _balance = 0;
+    batch;
+
+    withdrawn(value) {
+        if (this._balance >= value){
+            this._balance -= value;
+            return value;
+        };
+    };
+
+    deposit(value) {
+        if (value > 0){
+            this._balance += value;
+        };
+    };
+};
--- Index.js
+++ Index.js
@@ -1,27 +1,5 @@
-class Client {
-    name;
-    id;
-  
-};
-
-class Account {
-    _balance = 0;
-    batch;
-
-    withdrawn(value) {
-        if (this._balance >= value){
-            this._balance -= value;
-            return value;
-        };
-    };
-
-    deposit(value) {
-        if (value > 0){
-            this._balance += value;
-        };
-    };
-};
-
+import {Client} from "./Client.js"
+import {Account} from "./Account.js"
 // initial balance
 const robertoAccount = new Account();
 robertoAccount.batch = 1001;
