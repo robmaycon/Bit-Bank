0897b52da90f738bfc7348b52ebf296f0845e009
Roberto Maycon <robertomaycon@hotmail.com>
2021-01-13 18:39:30 +1300

    organizing code

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 import {Client} from "./Client.js"
 import {Account} from "./Account.js"
+
 // initial balance
 const robertoAccount = new Account();
 robertoAccount.batch = 1001;
@@ -7,14 +8,16 @@ console.log(robertoAccount);
 
 // Deposit
 robertoAccount.deposit(100);
+console.log(robertoAccount);
 robertoAccount.deposit(100);
+console.log(robertoAccount);
 robertoAccount.deposit(100);
 console.log(robertoAccount);
 
 //withdrawn
 const withdrawnValue = robertoAccount.withdrawn(50);
 console.log(`The Withdrawn Value was NZ$${withdrawnValue}`);
-
+console.log(robertoAccount);
 
 
 
